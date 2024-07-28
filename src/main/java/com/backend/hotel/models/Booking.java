9f085bbc9cabdf8e760a383c415e247f3ee34ea8
Shivam Bhanu <shivambhanu6924@gmail.com>
2024-07-28 12:52:22 +0530

    Created Booking model

---
+++ src/main/java/com/backend/hotel/models/Booking.java
@@ -0,0 +1,32 @@
+package com.backend.hotel.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "bookings")
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "hotel_id", nullable = false)
+    private Hotel hotel;
+
+    @Column(nullable = false)
+    private LocalDate bookingDate;
+
+    private LocalDate checkOutDate;
+}
